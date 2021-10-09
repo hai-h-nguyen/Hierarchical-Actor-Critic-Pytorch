c62a6b1ea63c7d866a6806a583b27db3be111f46
hhn1n15 <hainhbkhn@gmail.com>
2021-10-09 15:23:58 -0400

    add stuff

--- hac/domains/__init__.py
+++ hac/domains/__init__.py
@@ -1,45 +1,7 @@
 from gym.envs.registration import register
 
 register(
-    id='car-flag-hac-v0',
-    entry_point='domains.domains.car_flag:CarEnv',
-    max_episode_steps=160,
-)
-
-register(
-    id='bump-hac-v0',
-    entry_point='hac.domains.bump:BumpEnv',
-    max_episode_steps=160,
-)
-
-register(
-    id='ant-reacher-hac-v0',
-    entry_point='hac.domains.ant_reacher:AntEnv',
-    max_episode_steps=160,
-)
-
-register(
-    id='ur5-reacher-hac-v0',
+    id='ur5-reacher-v0',
     entry_point='hac.domains.ur5_reacher:Ur5Env',
-    max_episode_steps=160,
-)
-
-
-## HAC version
-register(
-    id='car-flag-hac-v1',
-    entry_point='hac.domains.car_flag_hac:CarEnv',
-    max_episode_steps=160,
-)
-
-register(
-    id='ant-reacher-hac-v1',
-    entry_point='hac.domains.ant_reacher_hac:AntEnv',
-    max_episode_steps=1000,
-)
-
-register(
-    id='car-flag-hac-v2',
-    entry_point='hac.domains.car_flag_hac_diff:CarEnv',
     max_episode_steps=160,
 )
